8e9a8722511c360bd4be9742a5a0de0034699e48
Chris Gray <cgray@icsfl.com>
2018-01-14 23:18:25 -0500

    Renamed Debt to DebtCalculatorApp in order to make way for a Debt class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import DebtCalculatorApp from './Debt.js';
+import DebtCalculatorApp from './DebtCalculatorApp.js';
 
 class App extends Component {
   render() {
